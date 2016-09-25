74a9ecace4ee6350a5e95f547a58b6a8075c59a7
William <wilbjo@student.chalmers.se>
2016-09-25 21:15:53 +0200

    Sprint 1 Development branch pushad till master

---
+++ android1337/app/src/main/java/com/android1337/Sentence.java
@@ -0,0 +1,25 @@
+package com.android1337;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Fedh on 2016-09-19.
+ */
+public class Sentence {
+    public String preWord = "";
+    public List<String> wordList= new ArrayList<String>();
+    public String postWord = "";
+    public Sentence() {
+
+    }
+    public Sentence(String preWord, List<String> wordList, String postWord) {
+        this.preWord = preWord;
+        this.postWord = postWord;
+        this.wordList = wordList;
+    }
+
+    public String toString() {
+        return preWord+"______"+postWord;
+    }
+}
