9b875ebcafe9ba7292c199c98818a5ca72b417f6
Victor Dahlberg <victord@student.chalmers.se>
2016-10-09 20:15:55 +0200

    Ny Settingsklass som representerar om du är inloggad eller inte.

--- android1337/app/src/main/java/com/android1337/MainMenu.java
+++ android1337/app/src/main/java/com/android1337/MainMenu.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
 import static com.android1337.R.id.multiPlayerButton;
 import static com.android1337.R.id.settingsButton;
 import static com.android1337.R.id.start;
@@ -28,7 +29,7 @@ public class MainMenu extends AppCompatActivity
         prefs = getSharedPreferences(PREF_FILE_NAME, MODE_PRIVATE);
 
         settingsButton = (ImageButton) findViewById(R.id.settingsButton);
-        settingsButton.setOnClickListener(v -> startActivity(new Intent(MainMenu.this, LoginMenu.class)));
+        settingsButton.setOnClickListener(v -> startActivity(new Intent(MainMenu.this, SettingsActivity.class)));
 
         singlePlayerButton = (ImageButton) findViewById(R.id.sinlgePlayerButton);
         singlePlayerButton.setOnClickListener(v -> startActivity(new Intent(MainMenu.this, ChooseGameActivity.class)));
@@ -38,7 +39,7 @@ public class MainMenu extends AppCompatActivity
             if(prefs.getBoolean("active", false)){
                 //startActivity(;new Intent(MainMenu.this, MultiPlayerEnter.class));
             } else {
-                startActivity(new Intent(MainMenu.this, LoginMenu.class));
+                startActivity(new Intent(MainMenu.this, SignupMenu.class));
             }
 
         });
